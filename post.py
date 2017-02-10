9ee75064dfe4f05758cc0329b84e3a662185f241
TestSmirk <d.kc@qq.com>
2017-02-10 12:00:10 +0800

    引入队列 线程上传图片.

--- post.py
+++ post.py
@@ -1,10 +1,38 @@
+import os
+import re
+import threading
+
 import requests
 
-if __name__ == '__main__':
-    while True:
-        r = requests.post("https://sm.ms/api/upload", files={'smfile': open('pycharm.png', 'rb')},
-                          data={"format": "xml"})
-        print(r.text)
+import json
 
-        # r = requests.post("http://192.168.1.217:8001/a/baseinfo",files={'head_image': open('pycharm.png','rb')},data={"_id":"5823d1954406b7368e2dd76c"})
-        # print(r.text)
+import queue
+
+
+def upload_file(file):
+    file = str(file)
+    r = requests.post("https://sm.ms/api/upload", files={'smfile': open(file, 'rb')},
+                      data={"format": "json"})
+    dict = json.loads(r.text)
+    if str(dict['code']) == "success":
+        print(dict['data']["url"])
+    else:
+        print("false")
+
+threads = []
+t1 = threading.Thread(target=upload_file,args="")
+def getFiles(dir):
+    for root, dirs, files in os.walk(dir):
+        for name in files:
+            if re.match('jpg|.png', name[-4:]):
+                print(root)
+                q = queue.Queue()
+                q.put(root + "/" + name)
+                upload_file(root + "/" + name)
+                #     print(os.path.join(root, name))
+
+
+getFiles("/home/bzby/")
+
+# r = requests.post("http://192.168.1.217:8001/a/baseinfo",files={'head_image': open('pycharm.png','rb')},data={"_id":"5823d1954406b7368e2dd76c"})
+# print(r.text)
